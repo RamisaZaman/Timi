46c446b21a08e7d338c2d0fe0f736f7233daeaa8
Ramisa Zaman <rrzaman@asu.edu>
2025-03-19 23:00:04 -0700

    Move Firebase configuration to environment variables

--- firebase.ts
+++ firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4Ualvj-YK0fuZUIny256LiaBqOpCpD2k",
-  authDomain: "ai-project-timi.firebaseapp.com",
-  projectId: "ai-project-timi",
-  storageBucket: "ai-project-timi.firebasestorage.app",
-  messagingSenderId: "1005809320177",
-  appId: "1:1005809320177:web:610a3aa59ff57dc79b6950",
-  measurementId: "G-9R5YDYSZXR"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
